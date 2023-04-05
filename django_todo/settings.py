7ff266cdbb214ecb6fc43bcf8dba997ed388222d
digimori <hanaorikuu@gmail.com>
2023-04-05 13:06:49 +0000

    Secret keys and other env variables added

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import dj_database_url
 
@@ -22,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+8d3(6%ykq-@gh^0k))03k4r63#9z=%eh%oraf-^5e!yh7@b*-'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'ZKkK50Jax9C0KhwKc289')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('djangotodolistapp.herokuapp.com')]
 
 
 # Application definition
